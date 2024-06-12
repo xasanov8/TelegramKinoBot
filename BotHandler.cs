f4d1382a96159c4629d1b138a80aea8957ded986
AbdullohDev07 <gamesupro01@gmail.com>
2024-06-12 13:33:35 +0500

    Add project files.

---
+++ BotHandler.cs
@@ -0,0 +1,78 @@
+﻿using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+
+public class BotHandler
+{
+    public string botToken { get; set; }
+    public BotHandler(string token)
+    {
+        botToken = token;
+    }
+
+    public async Task BotHandle()
+    {
+        var botClient = new TelegramBotClient(botToken);
+
+        using CancellationTokenSource cts = new();
+
+        ReceiverOptions receiverOptions = new()
+        {
+            AllowedUpdates = Array.Empty<UpdateType>()
+        };
+
+        botClient.StartReceiving(
+            updateHandler: HandleUpdateAsync,
+            pollingErrorHandler: HandlePollingErrorAsync,
+            receiverOptions: receiverOptions,
+            cancellationToken: cts.Token
+        );
+
+        var me = await botClient.GetMeAsync();
+
+        Console.WriteLine($"Start listening for @{me.Username}");
+        Console.ReadLine();
+
+        cts.Cancel();
+    }
+    public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        if (update.Message is not { } message)
+            return;
+
+        var getchatmember = await botClient.GetChatMemberAsync("@Abduvahobov09", update.Message.From.Id);
+        if (getchatmember.Status.ToString() == "Left" || getchatmember.Status.ToString() == null || getchatmember.Status.ToString() == "null" || getchatmember.Status.ToString() == "")
+        {
+            InlineKeyboardMarkup inlineKeyboard = new(new[]
+                    {
+                    new []
+                    {
+                        InlineKeyboardButton.WithUrl(text: "Canale 1", url: "https://t.me/Abduvahobov09"),
+                    },
+                });
+            await botClient.SendTextMessageAsync(
+                chatId: message.Chat.Id,
+                text: "Before use the bot you must follow this channels.\nWhen you are ready, click -> /home <- to continue",
+                replyMarkup: inlineKeyboard,
+                cancellationToken: cancellationToken);
+            return;
+
+        }
+
+    }
+        public async Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+        }
+
+ }
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string token = "6870782628:AAEH_4ldeBRsAiqg67mcGgEoNNH63iDdppw";
+
+        BotHandler handle = new BotHandler(token);
+
+        try
+        {
+            await handle.BotHandle();
+        }
+        catch
+        {
+            await handle.BotHandle();
+        }
+    }
+}
